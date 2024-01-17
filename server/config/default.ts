8741ef49259511bc94069595c92b9677a11fa520
sabrexghosh@gmail.com <96774134+Night3y3@users.noreply.github.com>
2024-01-17 18:14:50 +0530

    server created and working with a single route

---
+++ server/config/default.ts
@@ -0,0 +1,5 @@
+export default {
+  myPort: 3000,
+  dbUri: "mongodb://localhost:27017/mydb-img-cloud",
+  logLevel: "info",
+};
---
+++ server/src/routes/index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/health", (_, res) => {
+  res.sendStatus(200);
+});
+
+export default router;
